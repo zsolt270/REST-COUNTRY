18b5301f813a3caedd82667f5fbfeca3a94a7a93
zsolt270 <zsolt270000@gmail.com>
2024-07-16 17:02:42 +0200

    updates

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,19 +1,23 @@
-// import styles from "./Select.module.css";
+import styles from "./css.modules/Select.module.css";
+import { useState } from "react";
 
 export default function Select() {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<select className='form-select-sm'>
-			<option
-				selected
-				hidden
-			>
-				Filter by Region
-			</option>
-			<option value='Africa'>Africa</option>
-			<option value='America'>America</option>
-			<option value='Asia'>Asia</option>
-			<option value='Europe'>Europe</option>
-			<option value='Oceania'>Oceania</option>
-		</select>
+		<div></div>
+		// px-4 py-3
+		// <select className={styles.customeSelect}>
+		// 	<option
+		// 		selected
+		// 		hidden
+		// 	>
+		// 		Filter by Region
+		// 	</option>
+		// 	<option value='Africa'>Africa</option>
+		// 	<option value='America'>America</option>
+		// 	<option value='Asia'>Asia</option>
+		// 	<option value='Europe'>Europe</option>
+		// 	<option value='Oceania'>Oceania</option>
+		// </select>
 	);
 }

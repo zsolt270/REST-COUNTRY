3dd3bb2d48ab13ae9dcf7681b5e75b948b66c440
zsolt270 <zsolt270000@gmail.com>
2024-07-30 11:41:05 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,43 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./main.css";
 import HomePage from "./pages/HomePage";
+import App from "./App.tsx";
 import ErrorPage from "./pages/ErrorPage";
 import DetailsPage from "./pages/DetailsPage";
 import { ThemeContextProvider } from "./services/providers/themeContext.tsx";
 
 const router = createBrowserRouter([
 	{
+		// path: "/",
 		path: "/REST-COUNTRY/",
-		element: <HomePage />,
-		errorElement: <ErrorPage />,
-	},
-	{
-		path: "/REST-COUNTRY/country/:country",
-		element: <DetailsPage />,
+		element: <App />,
+		children: [
+			{
+				path: "/REST-COUNTRY/",
+				element: <HomePage />,
+				errorElement: <ErrorPage />,
+			},
+			{
+				path: "/REST-COUNTRY/country/:country",
+				element: <DetailsPage />,
+			},
+		],
 	},
 ]);
+
+// const router = createBrowserRouter([
+// 	{
+// 		// path: "/",
+// 		path: "/REST-COUNTRY/",
+// 		element: <HomePage />,
+// 		errorElement: <ErrorPage />,
+// 	},
+// 	{
+// 		// path: "/country/:country",
+// 		path: "/REST-COUNTRY/country/:country",
+// 		element: <DetailsPage />,
+// 	},
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
